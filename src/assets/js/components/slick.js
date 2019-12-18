837c080e18ad87b0c9148f6435f47365a136bbaa
MedoffSergey <mrgserega@mail.ru>
2019-12-18 21:04:01 +0300

    media fix

--- src/assets/js/components/slick.js
+++ src/assets/js/components/slick.js
@@ -4,8 +4,8 @@ portfolio.slick({
     arrows: false,
     dots: true,
     autoplay: true,
-    autoplaySpeed: 2000,
-    speed: 2500,
+    autoplaySpeed: 2500,
+    speed: 200,
     slidesToShow: 1,
     slidesToScroll: 1,
     fade: true, //плавный переход
